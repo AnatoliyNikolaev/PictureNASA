51c3e7a280cecf122d15b2fc5d6ea15de39098c0
AnatoliyNikolaev <nik.ay@mail.ru>
2023-11-26 17:34:51 +0300

    addded all

---
+++ NasaPicture/src/main/java/MyTelegramBot.java
@@ -0,0 +1,68 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+
+    public static final String BOT_TOKEN = "5868379241:AAFBVhEGVS2c0_jB6jAllT9He3t30fBjqA";
+
+    public static final String BOT_USERNAME = "NikkiNassa_Bot";
+
+    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=DgtY6e3nOKdvN8gfrhYeHxa8kdvGIxFjFdstfUJo";
+
+    public static long chat_id;
+
+    public MyTelegramBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(this);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            chat_id = update.getMessage().getChatId();
+            switch (update.getMessage().getText()) {
+                case "/help":
+                    sendMessage("Привет, я бот NASA! Я высылаю ссылки на картинки по запросу. " +
+                            "Напоминаю, что картинки на сайте NASA обновляются раз в сутки");
+                    break;
+                case "/give":
+                    try {
+                        sendMessage(Utils.getUrl(URI));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                default:
+                    sendMessage("Я не понимаю :(");
+            }
+        }
+    }
+
+
+    private void sendMessage(String messageText) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chat_id);
+        message.setText(messageText);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
